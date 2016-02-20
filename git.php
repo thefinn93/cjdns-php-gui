d9f9068db165b255420daa8826fae18666c628de
Kevin Hagerty <kevin@khagerty.com>
2016-02-20 15:22:03 -0500

    Fix php notices, truncate git head to 40 chars

--- git.php
+++ git.php
@@ -1,11 +1,11 @@
 <? include("config.inc.php");
-$cjdns_git = exec("git ls-remote $cjdnsgit origin -h refs/heads/master")
-$self_git = exec("git ls-remote origin -h refs/heads/master").substr(0,40);
+$cjdns_git = substr(exec("git ls-remote $cjdnsgit origin -h refs/heads/master"),0,40);
+$self_git = substr(exec("git ls-remote origin -h refs/heads/master"),0,40);
 $out = array();
-$out["self"] = array("current" => trim(file_get_contents(".git/refs/heads/master")), "latest" => $self_git[0]->sha);
-$out["cjdns"] = array("current" => trim(file_get_contents("$cjdnsgit/.git/refs/heads/master")), "latest" => $cjdns_git[0]->sha);
+$out["self"] = array("current" => trim(file_get_contents(".git/refs/heads/master")), "latest" => $self_git);
+$out["cjdns"] = array("current" => trim(file_get_contents("$cjdnsgit/.git/refs/heads/master")), "latest" => $cjdns_git);
 
-if($_REQUEST['action'] == "pull") {
+if( isset($_REQUEST['action']) && $_REQUEST['action'] == "pull") {
     $out["pull"] = array();
     exec("git pull", $out["pull"]);
 }
